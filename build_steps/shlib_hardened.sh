21ffe6be7a5cc2a907d69f1d093590ffd2b8fc5b
Alexander Drozdov <adrozdoff@gmail.com>
2017-08-07 12:36:34 +1000

    Removed static version

--- build_steps/shlib_hardened.sh
+++
@@ -1,8 +0,0 @@
-source /hbb/activate_func.sh
-export O3_ALLOWED=false
-activate_holy_build_box /hbb_shlib_hardened \
-        "-ffunction-sections -fdata-sections -fstack-protector -D_FORTIFY_SOURCE=2 -fPIE" \
-        "-static-libstdc++ -Wl,--gc-sections -pie -Wl,-z,relro" \
-        "-ffunction-sections -fdata-sections -fstack-protector -D_FORTIFY_SOURCE=2 -fPIC" \
-        "-ffunction-sections -fdata-sections -fstack-protector -D_FORTIFY_SOURCE=2 -fPIC" \
-        "-static-libstdc++"
